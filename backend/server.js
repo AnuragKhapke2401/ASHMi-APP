11f9c841ef60de443ae7eeed6e878e0fed08b527
AnuragKhapke2401 <anuragkhapke2003@gmail.com>
2025-04-24 00:39:44 +0530

    server.js error fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./db');
-const redis = require('./redis');
+const db = require('./config/db');
+const redis = require('./config/redis');
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
